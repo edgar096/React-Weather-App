3551d8f051abce2a38135dc3074988ad0955b4f1
edgar096 <edgar.goncalves96@gmail.com>
2023-05-30 14:28:58 +0100

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ import MapWidget from './Map/MapWidget';
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState(null);
+  const [position, setPosition] = useState([]);
+
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=06a0ddef3e13e908cd7e8f750a12cd78`;
+
   useEffect(() => {
     if (location != null) {
       fetch(url)
@@ -18,10 +21,12 @@ function App() {
         });
     }
   }, [location]);
+
   const handleSubmitLocation = (e) => {
     e.preventDefault();
     setLocation(e.target.locationSubmit.value);
   };
+
   return (
     <>
       <form action="submit" onSubmit={handleSubmitLocation}>
--- src/WeatherData/WeatherData.jsx
+++ src/WeatherData/WeatherData.jsx
@@ -6,7 +6,7 @@ const WeatherData = ({ data }) => {
       <li>Feels Like: {Math.round(data.main.feels_like - 273.15)}ºC</li>
       <li>
         Minimum: {Math.round(data.main.temp_min - 273.15)}ºC //Maximum:
-        {Math.round(data.main.temp_max - 273.15)}ºC{' '}
+        {Math.round(data.main.temp_max - 273.15)}ºC
       </li>
       <li>Current Humidity: {data.main.humidity}%</li>
     </ul>
