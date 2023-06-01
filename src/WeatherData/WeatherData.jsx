ce8151c6d6cf9ee9f72b9c11a96f52798a6aea94
edgar096 <edgar.goncalves96@gmail.com>
2023-06-01 16:43:44 +0100

    1 file modified

--- src/WeatherData/WeatherData.jsx
+++ src/WeatherData/WeatherData.jsx
@@ -3,16 +3,7 @@ import { Grid, SimpleGrid } from '@mantine/core';
 const WeatherData = ({ data }) => {
   return (
     <>
-      <SimpleGrid cols={2} spacing="xl">
-        <span>Temperature: {Math.round(data.main.temp)}ºC</span>
-        <span>Feels Like: {Math.round(data.main.feels_like)}ºC</span>
-        <SimpleGrid cols={3} spacing="xl">
-          <span>Minimum: {Math.round(data.main.temp_min)}ºC</span>
-          <span>Maximum:{Math.round(data.main.temp_max)}ºC</span>
-        </SimpleGrid>
-        <span> Current Humidity: {data.main.humidity}%</span>
-      </SimpleGrid>
-      {/* <Grid gutter={5} gutterXs="md" gutterMd="xl" gutterXl={50}>
+      <Grid gutter={5} gutterXs="md" gutterMd="xl" gutterXl={50}>
         <Grid.Col span={6}>
           Temperature: {Math.round(data.main.temp)}ºC
         </Grid.Col>
@@ -31,7 +22,7 @@ const WeatherData = ({ data }) => {
           </Grid>
         </Grid.Col>
         <Grid.Col span={6}>Current Humidity: {data.main.humidity}%</Grid.Col>
-      </Grid> */}
+      </Grid>
     </>
   );
 };
