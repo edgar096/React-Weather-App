56ffc32b37b75415eedd9b78dabcb5f8537c0460
edgar096 <edgar.goncalves96@gmail.com>
2023-06-15 11:12:41 +0100

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import MapWidget from './Map/MapWidget';
 import { Grid, MantineProvider } from '@mantine/core';
 import LocationForm from './UI/Form/LocationForm';
 import ErrorBoundary from './UI/ErrorBoundary/ErrorBoundary';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import AutoMapRecenter from './Map/AutoMapRecenter';
 
 function App() {
   const apiKey = import.meta.env.VITE_API_KEY;
@@ -36,12 +39,24 @@ function App() {
   return (
     <>
       <LocationForm handler={handleSubmitLocation} />
-      <ErrorBoundary fallback={<h1>test</h1>}>
+      <ErrorBoundary fallback={<p>Something went wrong</p>}>
         {data.cod && (
           <>
-            <h1>Location:{data.name}</h1>
-            <WeatherData data={data} />
-            <MapWidget data={data} />
+            {/* <h1>Location:{data.name}</h1> */}
+            {/* <WeatherData data={data} /> */}
+            <MapContainer
+              center={[data.coord.lat, data.coord.lon]}
+              zoom={13}
+              scrollWheelZoom={false}
+              style={{ width: '100%', height: 'calc(100vh - 4rem)' }}
+            >
+              <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[data.coord.lat, data.coord.lon]}></Marker>
+              <AutoMapRecenter position={[data.coord.lat, data.coord.lon]} />
+            </MapContainer>
           </>
         )}
       </ErrorBoundary>
--- src/UI/ErrorBoundary/ErrorBoundary.jsx
+++ src/UI/ErrorBoundary/ErrorBoundary.jsx
@@ -5,18 +5,18 @@ class ErrorBoundary extends React.Component {
     this.state = { hasError: false };
   }
 
-  static getDerivedStateFromError(error) {
-    // Update state so the next render will show the fallback UI.
-    return { hasError: true };
-  }
+  // static getDerivedStateFromError(error) {
+  //   // Update state so the next render will show the fallback UI.
+  //   return { hasError: true };
+  // }
 
   componentDidCatch(error, info) {
     // Example "componentStack":
     //   in ComponentThatThrows (created by App)
     //   in ErrorBoundary (created by App)
     //   in div (created by App)
-    //   in App
-    logErrorToMyService(error, info.componentStack);
+    //in App
+    this.setState({ hasError: true });
   }
 
   render() {
