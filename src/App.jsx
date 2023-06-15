2f3b8343d3b0337042277f19355e8d86e9f14f66
edgar096 <edgar.goncalves96@gmail.com>
2023-06-15 16:53:39 +0100

    1 file modified

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import ErrorBoundary from './UI/ErrorBoundary/ErrorBoundary';
 import 'leaflet/dist/leaflet.css';
 import { MantineProvider, Text } from '@mantine/core';
 import MapWidget from './Map/MapWidget';
-
+import FallbackComponent from './UI/ErrorBoundary/FallbackComponent';
 function App() {
   const apiKey = import.meta.env.VITE_API_KEY;
 
@@ -39,7 +39,7 @@ function App() {
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <>
         <LocationForm handler={handleSubmitLocation} />
-        <ErrorBoundary fallback={<h1>error</h1>} key={data.cod}>
+        <ErrorBoundary fallback={<p>test</p>} key={data.cod}>
           {data.cod && (
             <>
               <h1>Location:{data.name}</h1>
