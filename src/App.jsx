de50f1e23a648a2f4c7e27b3b982dcd159cce64d
edgar096 <edgar.goncalves96@gmail.com>
2023-06-15 12:54:58 +0100

    2 files modified

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import { useState, useEffect } from 'react';
 import './App.css';
 import 'leaflet/dist/leaflet.css';
 import WeatherData from './WeatherData/WeatherData';
-import MapWidget from './Map/MapWidget';
-import { Grid, MantineProvider } from '@mantine/core';
 import LocationForm from './UI/Form/LocationForm';
 import ErrorBoundary from './UI/ErrorBoundary/ErrorBoundary';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import AutoMapRecenter from './Map/AutoMapRecenter';
+import { MantineProvider, Text } from '@mantine/core';
+import MapWidget from './Map/MapWidget';
 
 function App() {
   const apiKey = import.meta.env.VITE_API_KEY;
@@ -21,14 +19,16 @@ function App() {
       const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${apiKey}`;
 
       let response = await fetch(url);
-      if (!response.ok) {
-        throw new Error('Bad API Response');
-      }
+      // if (!response.ok) {
+      //   throw new Error('Bad API Response');
+      // }
       let urlData = await response.json();
       setData(urlData);
+      console.log(response);
     } catch (error) {
+      //console.log(error);
       setError(error);
-      throw error;
+      // throw error;
     }
   }
 
@@ -37,30 +37,20 @@ function App() {
     getData(e.target.locationSubmit.value);
   };
   return (
-    <>
-      <LocationForm handler={handleSubmitLocation} />
-      <ErrorBoundary fallback={<p>Something went wrong</p>}>
-        {data.cod && (
-          <>
-            {/* <h1>Location:{data.name}</h1> */}
-            {/* <WeatherData data={data} /> */}
-            <MapContainer
-              center={[data.coord.lat, data.coord.lon]}
-              zoom={13}
-              scrollWheelZoom={false}
-              style={{ width: '100%', height: 'calc(100vh - 4rem)' }}
-            >
-              <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-              />
-              <Marker position={[data.coord.lat, data.coord.lon]}></Marker>
-              <AutoMapRecenter position={[data.coord.lat, data.coord.lon]} />
-            </MapContainer>
-          </>
-        )}
-      </ErrorBoundary>
-    </>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <>
+        <LocationForm handler={handleSubmitLocation} />
+        <ErrorBoundary fallback={<h1>error</h1>}>
+          {data.cod && (
+            <>
+              <h1>Location:{data.name}</h1>
+              <WeatherData data={data} />
+              <MapWidget data={data} />
+            </>
+          )}
+        </ErrorBoundary>
+      </>
+    </MantineProvider>
   );
 }
 
--- src/UI/ErrorBoundary/ErrorBoundary.jsx
+++ src/UI/ErrorBoundary/ErrorBoundary.jsx
@@ -5,10 +5,10 @@ class ErrorBoundary extends React.Component {
     this.state = { hasError: false };
   }
 
-  // static getDerivedStateFromError(error) {
-  //   // Update state so the next render will show the fallback UI.
-  //   return { hasError: true };
-  // }
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
 
   componentDidCatch(error, info) {
     // Example "componentStack":
